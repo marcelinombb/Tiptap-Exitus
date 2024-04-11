c9f2c6483cc8104241b560b627233fb00a20f96b
Thiago Sampaio <thiagooliveira1039@gmail.com>
2024-04-11 17:09:46 -0300

    Tecla Tab funcionando, faltando ShiftTab

--- src/extensions/tab/tab.ts
+++ src/extensions/tab/tab.ts
@@ -1,22 +1,14 @@
-// @ts-nocheck
-import { Node } from '@tiptap/core'
+//@ts-nocheck
+import { type Editor, Node, NodePos } from '@tiptap/core'
 
-
-function addTab ({ editor }) {
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Tab') {
-         
-            event.preventDefault();
-            editor.commands.insertContentAt(editor.view.state.selection.$anchor.pos, `<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>`, {
-                updateSelection: true,
-                parseOptions: {
-                  preserveWhitespace: 'full'
-                }
-              })
-            }
-        })
+function addTab(editor: Editor) {
+  return editor.commands.insertContentAt(editor.view.state.selection.$anchor.pos, '<span>        </span>', {
+    updateSelection: true,
+    parseOptions: {
+      preserveWhitespace: 'full'
     }
-
+  })
+}
 
 export const Tab = Node.create({
   name: 'teclatab',
@@ -29,27 +21,66 @@ export const Tab = Node.create({
 
   atom: true,
 
-
   addCommands() {
     return {
-      tabIndent: () => ({ tr, state, dispatch, editor }) => {
-        addTab({ editor });
-        return true;
-      },
-        
-        tabOutdent: () => ({ tr, state, dispatch, editor }) => {
-            
-            console.log("tirou o espaço");
+      tabIndent:
+        () =>
+        ({ tr, state, dispatch, editor }) => {
+          const { selection } = state
+          tr = tr.setSelection(selection)
+          tr = addTab(tr, editor)
+
+          if (tr.docChanged) {
+            // eslint-disable-next-line no-unused-expressions
+            dispatch && dispatch(tr)
             return true
+          }
+
+          editor.chain().focus().run()
+
+          return false
         },
+      tabOutdent: () => () => {
+        return true
+      }
     }
-},
-addKeyboardShortcuts() {
+  },
+  parseHTML() {
+    return [
+      {
+        tag: 'span'
+      }
+    ]
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ['span', HTMLAttributes, 0]
+  },
+  addAttributes() {
     return {
-      Tab: () => { if (!(this.editor.isActive('bulletList') || this.editor.isActive('orderedList'))) return this.editor.commands.tabIndent() },
-      'Shift-Tab': () => { if (!(this.editor.isActive('bulletList') || this.editor.isActive('orderedList'))) return this.editor.commands.tabOutdent() },
+      class: {
+        default: ''
+      }
     }
   },
 
-})
+  addKeyboardShortcuts() {
+    return {
+      Tab: () => {
+        if (!(this.editor.isActive('bulletList') || this.editor.isActive('orderedList'))) {
+          return addTab(this.editor)
+        }
+        return false
+      },
+      'Shift-Tab': () => {
+        if (!(this.editor.isActive('bulletList') || this.editor.isActive('orderedList'))) {
+          const pos = this.editor.view.state.selection.$anchor
+          console.log(pos)
 
+          const myNodePos = new NodePos(pos, this.editor)
+          console.log(myNodePos.before)
+        }
+        return false
+      }
+    }
+  }
+})
