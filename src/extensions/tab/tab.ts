1c0d44bf23ff4bbf6583f42a5649ecbe7f948f50
Thiago Sampaio <thiagooliveira1039@gmail.com>
2024-04-12 12:02:02 -0300

    continuação do quase lá do shift Tab 12/03

--- src/extensions/tab/tab.ts
+++ src/extensions/tab/tab.ts
@@ -1,8 +1,9 @@
 //@ts-nocheck
-import { type Editor, Node, NodePos } from '@tiptap/core'
+import { type Editor, Node } from '@tiptap/core'
+import { mergeAttributes } from '@tiptap/core'
 
 function addTab(editor: Editor) {
-  return editor.commands.insertContentAt(editor.view.state.selection.$anchor.pos, '<span>        </span>', {
+  return editor.commands.insertContentAt(editor.view.state.selection.$anchor.pos, `<span class="ex-tab"></span>`, {
     updateSelection: true,
     parseOptions: {
       preserveWhitespace: 'full'
@@ -28,7 +29,7 @@ export const Tab = Node.create({
         ({ tr, state, dispatch, editor }) => {
           const { selection } = state
           tr = tr.setSelection(selection)
-          tr = addTab(tr, editor)
+          tr = addTab(editor)
 
           if (tr.docChanged) {
             // eslint-disable-next-line no-unused-expressions
@@ -45,15 +46,20 @@ export const Tab = Node.create({
       }
     }
   },
+
   parseHTML() {
     return [
       {
-        tag: 'span'
+        tag: 'span',
+        getAttrs: node => {
+          return (node as HTMLElement).className === 'ex-tab' && null
+        },
+        priority: 9999
       }
     ]
   },
   renderHTML({ HTMLAttributes }) {
-    return ['span', HTMLAttributes, 0]
+    return ['span', mergeAttributes(HTMLAttributes, { class: 'ex-tab' }), '\u00A0'.repeat(8)]
   },
   addAttributes() {
     return {
@@ -73,11 +79,18 @@ export const Tab = Node.create({
       },
       'Shift-Tab': () => {
         if (!(this.editor.isActive('bulletList') || this.editor.isActive('orderedList'))) {
-          const pos = this.editor.view.state.selection.$anchor
-          console.log(pos)
+          const selection = this.editor.view.state.selection
+          if (selection && selection.$anchor) {
+            const pos = selection.$anchor
 
-          const myNodePos = new NodePos(pos, this.editor)
-          console.log(myNodePos.before)
+            if (pos.nodeBefore && pos.nodeBefore.type.name === 'teclatab') {
+              this.editor.commands.clearContent(pos.nodeBefore)
+              return false
+            }
+
+            console.log(pos)
+            console.log(this.editor.isActive('teclatab'))
+          }
         }
         return false
       }
