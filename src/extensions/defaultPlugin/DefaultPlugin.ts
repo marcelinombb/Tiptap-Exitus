c66621c57561b87ce474f7cb60c4bc1b658bdc1f
marcelino.braga <dsoftware66@px.com.br>
2024-07-31 17:44:56 -0300

    refact:  remoção do plugin heading da lista de plugins padrões

--- src/extensions/defaultPlugin/DefaultPlugin.ts
+++ src/extensions/defaultPlugin/DefaultPlugin.ts
@@ -3,7 +3,6 @@ import { Document } from '@tiptap/extension-document'
 import Dropcursor from '@tiptap/extension-dropcursor'
 import Gapcursor from '@tiptap/extension-gapcursor'
 import HardBreak from '@tiptap/extension-hard-break'
-import Heading from '@tiptap/extension-heading'
 import HorizontalRule from '@tiptap/extension-horizontal-rule'
 import Paragraph from '@tiptap/extension-paragraph'
 import { Text } from '@tiptap/extension-text'
@@ -13,7 +12,7 @@ export class DefaultPlugin extends Plugin {
     return 'defaultplugin'
   }
   static get requires() {
-    return [Document, Text, Paragraph, Gapcursor, HorizontalRule, HardBreak, Dropcursor, Heading]
+    return [Document, Text, Paragraph, Gapcursor, HorizontalRule, HardBreak, Dropcursor]
   }
   init(): void {}
 }
